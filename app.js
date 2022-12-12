f92e223708b8b4c97bb978e2c8473e8b24f9f541
Gracias KPAKOSSOU <gracias.kpakossou@>
2022-12-12 12:31:37 +0100

    second

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-bodyParser = require("body-parser"),
-{ urlencoded, json } = require("body-parser"),
- app = express().use(bodyParser.json());
+const app = express();
 
 
 const PORT = 3000;
@@ -25,9 +23,9 @@ if (body.object === "page") {
 }); 
 app.get("/messaging-webhook", (req, res) => {
   
-      let mode = req.query["hub.mode"];
-      let token = req.query["hub.verify_token"];
-      let challenge = req.query["hub.challenge"];
+      let mode = req.query["suscribe"];
+      let token = req.query["110015668622854"];
+      let challenge = req.query["mytoken"];
     
       if (mode && token) {
         if (mode === "subscribe" && token === config.verifyToken) {
@@ -49,20 +47,6 @@ app.listen(PORT, (error) =>{
     }
 );
 
-function verifyRequestSignature(req, res, buf) {
-    var signature = req.headers["x-hub-signature-256"];
-  
-    if (!signature) {
-      console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
-    } else {
-      var elements = signature.split("=");
-      var signatureHash = elements[1];
-      var expectedHash = crypto
-        .createHmac("sha256", config.appSecret)
-        .update(buf)
-        .digest("hex");
-      if (signatureHash != expectedHash) {
-        throw new Error("Couldn't validate the request signature.");
-      }
-    }
-  }
+
+
+
