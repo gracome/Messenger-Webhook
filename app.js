c5e1994e6b6a6bf82cb0061d2d7e87c69293ac97
Gracias KPAKOSSOU <gracias.kpakossou@>
2022-12-13 12:29:15 +0100

    main

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const app = express();
-const request = require('request');
 
 
 const PORT = 3000;
-app.listen(PORT, (error) =>{
-    if(!error) {
-        console.log("Server is Successfully Running,and App is listening on port "+ PORT)
-    }else {
-        console.log("Error occurred, server can't start", error);
-    }
-}
-);
+
 
 app.post("/webhook", (req, res) => {
     let body = req.body;
@@ -23,26 +15,20 @@ app.post("/webhook", (req, res) => {
 if (body.object === "page") {
     body.entry.forEach(function(entry) {
 
+        
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
-      
-      
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
-      
-        // Check if the event is a message or postback and
-        // pass the event to the appropriate handler function
-        if (webhook_event.message) {
-          handleMessage(sender_psid, webhook_event.message);        
-        } else if (webhook_event.postback) {
-          handlePostback(sender_psid, webhook_event.postback);
-        }
         
-      });    
-
+      });
   
-}
+      
+      res.status(200).send('EVENT_RECEIVED');
+  
+    } else {
+      res.sendStatus(404);
+    }
+  
+  });
 app.get("/messaging-webhook", (req, res) => {
   
     let mode = req.query["hub.mode"];
@@ -56,62 +42,21 @@ app.get("/messaging-webhook", (req, res) => {
           res.status(200).send(challenge);
         } else {
           res.sendStatus(403);
-
+          
         }
       }
     });
    
 
   
-
-
-})
-
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-    let response;
-
-  // Check if the message contains text
-  if (received_message.text) {    
-
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running,and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
     }
-  }  
-  
-  // Sends the response message
-  callSendAPI(sender_psid, response); 
+);
 
-}
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
 
-}
 
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-     // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-  
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": "EAARKYqLdIJMBAEow3XhD8Rvun85i7JjwGap9sA9VLoZCtYx8frBX6s9GbZAVgh78np7WnLZCTZBrZBmNFPSQFkqv27bLXhNtXQtC2QMmF7Sb8qxWsivOn5HOv5Nx5cxHXv90rBCHILZAuQxwU1ADX1CANUdGkwo2cRgCisj4CFgrIyOLRKQC7iVuvwsoOxX6kZD" },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
-}
