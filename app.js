e743d529cdfaa2dbcd36c5d79b4c3637319ccb21
Gracias KPAKOSSOU <gracias.kpakossou@>
2022-12-22 12:59:21 +0100

    j

--- app.js
+++ app.js
@@ -30,14 +30,10 @@ if (body.object === "page") {
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
 
-        webhook_event.forEach(function(messaging){
-
-        let message= messaging[0]
-        console.log(message);
     
         // Emit the event to the messaging micro service
-         nrp.emit("NEW_MESSAGE", message);
-      });
+         nrp.emit("NEW_MESSAGE",  webhook_event);
+    
         });
  
         
